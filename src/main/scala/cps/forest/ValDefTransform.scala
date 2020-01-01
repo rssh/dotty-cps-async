f50c3294aecb496a8bb714fc4370477ceb5f0133
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2020-01-01 21:48:06 +0200

    implement valDef transformer

--- src/main/scala/cps/forest/ValDefTransform.scala
+++ src/main/scala/cps/forest/ValDefTransform.scala
@@ -18,23 +18,38 @@ class ValDefTransform[F[_]:Type, T:Type](f: Expr[T], dm:Expr[AsyncMonad[F]])
       val tastyTType = tType.unseal.tpe
       println("!!! val detected")
       println(s"f=${f.unseal}")
+      println(s"f.symbol=${f.unseal.symbol}")
       val ry = Async.rootTransform[F,TX](y,dm)
       println(s"ry=$ry")
       println(s"x=$x, tx=$tx, y=$y")
       if (ry.haveAwait) 
+         
          val py = ry.transformed
-         val fTransformed = '{ ${dm}.map($py)((a:$tx) => {val $x:$tx = a}) }
          val cpsBuild = new CpsChunkBuilder[F,Unit] {
+
+            def buildValDef[T](a:Expr[T]):Expr[Unit] =
+                 val oldValDef = extractValDef(f.asInstanceOf[Expr[Unit]])
+                 println("oldValDef.symbol=${oldValDef.symbol}")
+                 valDefBlock(newValDef(oldValDef.symbol,a.unseal))
+
+            def oldSymbol() = extractValDef(f.asInstanceOf[Expr[Unit]]).symbol
+
+            def fixNewIdent[A](ident:Ident, expr:Expr[A]):Term =
+                                substituteIdent(expr.unseal, oldSymbol(), ident)
+
             override def create() = 
-                fromFExpr('{ ${dm}.map($py)((a:$tx) => {val $x:$tx = a}) })
+                fromFExpr('{ ${dm}.map($py)((a:$tx) => ${buildValDef('a)}) })
+
+
             override def append[A:quoted.Type](e: CpsChunk[F,A]) =
                 // TODO: inject vlaDef into e
                 CpsChunk[F,A](Seq(),
                  '{ 
                     ${dm}.flatMap($py)((a:$tx) => 
                       {
-                        val $x: $tx = a
-                        ${e.toExpr}
+                        ${(let(('a).unseal)(
+                           ident => fixNewIdent(ident,e.toExpr)
+                         )).seal.asInstanceOf[Expr[F[A]]]}
                        })
                     })
                  }
@@ -43,4 +58,52 @@ class ValDefTransform[F[_]:Type, T:Type](f: Expr[T], dm:Expr[AsyncMonad[F]])
          val cpsBuild = CpsChunkBuilder.sync(f,dm) 
          CpsExprResult[F,T](f,cpsBuild,tType,false)
      
+  def newValDef(given qctx: QuoteContext)(symbol:qctx.tasty.Symbol, newRhs: qctx.tasty.Term): qctx.tasty.ValDef = {
+         import qctx.tasty.{_,given}
+          // note, that owner of symbol is old.
+         ValDef(symbol,Some(newRhs))
+         
+  }
+
+  def valDefBlock(given qctx:QuoteContext)(v:qctx.tasty.ValDef):Expr[Unit] = {
+    import qctx.tasty.{_,given}
+    Block(List(v),Literal(Constant(()))).seal.asInstanceOf[Expr[Unit]]
+  }
+
+  def extractValDef(given qctx:QuoteContext)(blockExpr:Expr[Unit]): qctx.tasty.ValDef = {
+    import qctx.tasty.{_,given}
+    blockExpr.unseal match {
+      case Block(stats,last) =>
+        stats.head match {
+          case v: ValDef => v
+          case _ => qctx.error("Block with ValDef as first statement expected",blockExpr)
+                  ???
+        }
+      case Inlined(call,binding,body) => extractValDef(body.seal.asInstanceOf[Expr[Unit]])
+      case _ => qctx.error("Block expected",blockExpr)
+                ??? 
+    }
+  }
+ 
+  // substitute identifier with the same part.
+
+
+  def substituteIdent(given qctx:QuoteContext)(tree: qctx.tasty.Term, 
+                           origin: qctx.tasty.Symbol, 
+                           newIdent: qctx.tasty.Ident): qctx.tasty.Term =
+     import qctx.tasty.{_,given}
+     import qctx.tasty._
+     import util._
+     val changes = new TreeMap() {
+        override def transformTerm(tree:Term)(given ctx: Context):Term =
+          tree match 
+            case ident@Ident(name) => if (ident.symbol == origin) {
+                                         newIdent
+                                      } else {
+                                         super.transformTerm(tree)
+                                      }
+            case _ => super.transformTerm(tree)
+     }
+     changes.transformTerm(tree)
+
 
