352b4420a7310a9f9a4b45a882e0509a31797941
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2020-01-06 08:14:22 +0200

    deleted tests, which not crash compiler

--- src/test/scala/cps/TestCBS1While.scala
+++ src/test/scala/cps/TestCBS1While.scala
@@ -10,47 +10,6 @@ import scala.util.Success
 class TestBS1While
 
 
-  @Test def tWhileC1_00(): Unit = 
-     val c = async[ComputationBound]{
-        val n = 10
-        var s = 0
-        var i = 0
-        while(i < n) 
-          s += i
-          i += 1
-        s
-     }
-     assert(c.run() == Success(45))
-
-
-  @Test def tWhileC1_10(): Unit = 
-     val c = async[ComputationBound]{
-        val n = 10
-        var s = 0
-        var i = 0
-        while(await( T1.cbBool(i < n) )) {
-          s += i
-          i += 1
-        }
-        s
-     }
-     assert(c.run() == Success(45))
-
-  @Test def tWhileC1_01(): Unit = 
-     val c = async[ComputationBound]{
-        val n = 10
-        var s = 0
-        var i = 0
-        while(i < n) {
-          val q = await(T1.cbi(i))
-          s += q
-          i += 1
-        }
-        s
-     }
-     assert(c.run() == Success(45))
-
-/*
  // Dotty crash.
  // TODO: minimize and submit bug.
  //
@@ -68,6 +27,5 @@ class TestBS1While
      }
      assert(c.run() == Success(45))
 
-*/
 
 
