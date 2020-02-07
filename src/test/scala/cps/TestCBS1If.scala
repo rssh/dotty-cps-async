24a1b33edbdbf819e7826c1e2a9309a05e61bd96
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2020-02-08 00:31:44 +0200

    minimising

--- src/test/scala/cps/TestCBS1If.scala
+++ src/test/scala/cps/TestCBS1If.scala
@@ -9,64 +9,6 @@ import scala.util.Success
 
 class TestBS1If
 
-  @Test def tIfC1_000(): Unit = 
-     val c = Async.transform[ComputationBound,Int]{
-        if (true) 1 else 2
-     }
-     assert(c.run() == Success(1))
-
-  @Test def tIfC1_001(): Unit = 
-     val c = Async.transform[ComputationBound,Int]{
-        if (true) 1 else await(T1.cbi(2))
-     }
-     assert(c.run() == Success(1))
-
-  @Test def tIfC1_001f(): Unit = 
-     val c = Async.transform[ComputationBound,Int]{
-        if (false) 1 else await(T1.cbi(2))
-     }
-     assert(c.run() == Success(2))
-
-  @Test def tIfC1_010(): Unit = 
-     val c = Async.transform[ComputationBound,Int]{
-        if (true) await(T1.cbi(1)) else 2
-     }
-     assert(c.run() == Success(1))
-
-  @Test def tIfC1_011(): Unit = 
-     val c = Async.transform[ComputationBound,Int]{
-        if (true) await(T1.cbi(1)) else await(T1.cbi(2))
-     }
-     assert(c.run() == Success(1))
-
-
-  @Test def tIfC1_100(): Unit = 
-     val c = Async.transform[ComputationBound,Int]{
-        if (await(T1.cbBool(true)))
-            2 
-        else 
-            3
-     }
-     assert(c.run() == Success(2))
-
-  @Test def tIfC1_100f(): Unit = 
-     val c = Async.transform[ComputationBound,Int]{
-        if (await(T1.cbBool(false)))
-            2 
-        else 
-            3
-     }
-     assert(c.run() == Success(3))
-  
-
-  @Test def tIfC1_111(): Unit = 
-     val c = Async.transform[ComputationBound,Int]{
-        if (await(T1.cbBool(true)))
-            await(T1.cbi(2))
-        else 
-            await(T1.cbi(3))
-     }
-     assert(c.run() == Success(2))
 
   @Test def tIfC1_inBlock(): Unit = 
      val c = Async.transform[ComputationBound,Int]{
@@ -80,13 +22,4 @@ class TestBS1If
        }
      }
      assert(c.run() == Success(5))
-
-  @Test def tIf_OneLeg_C00(): Unit = 
-     val c = async[ComputationBound]{
-       var x = 0
-       if (true)
-         x = 1
-       x+1
-     }
-     assert(c.run() == Success(2))
 
