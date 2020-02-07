39240b8f6297239a08d8a64e00e15581c47bc331
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2020-02-07 07:55:47 +0200

    minimizing

--- src/main/scala/cps/B.scala
+++ src/main/scala/cps/B.scala
@@ -20,25 +20,19 @@ object B {
     import qctx.tasty.{_,given}
     val q = qe.unseal.underlyingArgument
     val prev3 = List(Literal(Constant(())))
-    val x1Ident = extractIdent("x1",q)
     val x2Ident = extractIdent("x2",q)
     val plusSelect = extractSelect("x1",q)
     val lastExpr3 = Apply(plusSelect, List(x2Ident)).seal.asInstanceOf[Expr[T]]
     val r3=Expr.block(prev3.map(_.seal), '{ CBM.pure($lastExpr3) })
     val oldValDef1= extractValDefExpr("x2",q.seal).get
-    val prev1=List(Literal(Constant(())))
-    val next1=r3
-    val r1=Block(prev1 ++: List(oldValDef1), next1.unseal).seal.asInstanceOf[Expr[CB[T]]]
+    val r1=Block(prev3 ++: List(oldValDef1), r3.unseal).seal.asInstanceOf[Expr[CB[T]]]
     val oldValDef0 = extractValDefExpr("x1",q.seal).get
-    val prev0=List()
-    val next0=r1
-    val r0=Block(prev0 ++: List(oldValDef0), next0.unseal).seal.asInstanceOf[Expr[CB[T]]]
+    val r0=Block(List(oldValDef0), r1.unseal).seal.asInstanceOf[Expr[CB[T]]]
     r0
   }
 
   def extractValDefExpr(given qctx:QuoteContext)(name:String, code: Expr[Any]): Option[qctx.tasty.ValDef] =
    import qctx.tasty.{_,given}
-   println(s"extract valDef: code=${code.unseal}")
    code.unseal match {
      case Block(stats, last) =>
        stats.head match {
--- src/test/scala/cps/TestCBS1.scala
+++ src/test/scala/cps/TestCBS1.scala
@@ -1,15 +1,9 @@
 package cps
 
-import org.junit.{Test,Ignore}
-import org.junit.Assert._
-
-import scala.quoted._
-import scala.util.Success
-
 
 class TestBS1
 
-  @Test def testBadTree(): Unit =
+  def testBadTree(): Unit =
     val c = B.badTree[Int]{
          val x1 = 3
          val x2 = 4 
