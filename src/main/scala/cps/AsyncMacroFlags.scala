cd93d3c5d5dbf44a239106b9bf50fd295da172cb
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2020-02-29 15:16:04 +0200

    added AsycnMacroFlag as entity

---
+++ src/main/scala/cps/AsyncMacroFlags.scala
@@ -0,0 +1,22 @@
+package cps
+
+import scala.quoted._
+import scala.quoted.matching._
+
+
+case class AsyncMacroFlags(
+   printCode: Boolean = false,
+   printTree: Boolean = false,
+   debugLevel: Int = 0
+)
+
+given ValueOfExpr[AsyncMacroFlags]:
+   
+   def apply(x: Expr[AsyncMacroFlags])(using qctx: QuoteContext): Option[AsyncMacroFlags] =
+     x match 
+       case '{ AsyncMacroFlags(${Value(x)},${Value(y)},${Value(z)}) } =>
+                 Some(AsyncMacroFlags(x,y,z))
+       case _ => None
+           
+     
+
