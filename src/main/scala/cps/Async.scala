bc48e7f256db84c31e67a1dae1eaf7dc7644aacd
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2020-06-27 00:58:34 +0300

    minimized

--- src/main/scala/cps/Async.scala
+++ src/main/scala/cps/Async.scala
@@ -1,197 +1,49 @@
 package cps
 
-import scala.language.implicitConversions
-
-import scala.annotation._
 import scala.quoted._
-import scala.compiletime._
 
-import cps.forest._
-import cps.misc._
+trait CpsMonad[F[_]] 
 
-// erased disabled in current version
-// erased def await[F[_],T](f:F[T]):T = ???
+trait ComputationBound[T] 
 
-@compileTimeOnly("await should be inside async block")
-def await[F[_],T](f:F[T]):T = ???
+implicit object ComputationBoundMonad extends CpsMonad[ComputationBound] 
 
 inline def async[F[_]](using am:CpsMonad[F]): Async.InferAsyncArg[F] =
    new Async.InferAsyncArg[F]
-
-inline def async1[F[_]](using am:CpsMonad[F]): Async.InferAsyncArg1[F] =
-   new Async.InferAsyncArg1[F]
-
 
 object Async {
 
   class InferAsyncArg[F[_]](using am:CpsMonad[F]) {
 
-       inline def apply[T](inline expr: T):F[T] =
-            transform[F,T](expr)
+       inline def apply[T](inline expr: T):Unit =
+       ${
+         Async.checkPrintTypeImpl[F,T]('expr)
+        }
 
   }
 
-  class InferAsyncArg1[F[_]](using am:CpsMonad[F]) {
-       inline def apply[T](inline expr: T):F[T] = {
-            checkPrintType[F,T](expr)
-       }
-  }
 
-
-  inline def async[F[_]](using am:CpsMonad[F]): InferAsyncArg[F] =
-          new InferAsyncArg[F]
-
-  inline def transform[F[_], T](inline expr: T): F[T] =
-    ${ 
-        Async.transformImpl[F,T]('expr)
-     } 
-
-  inline def checkPrintType[F[_], T](inline expr: T): F[T] =
-    ${ 
-        Async.checkPrintTypeImpl[F,T]('expr)
-     } 
-
-  def checkPrintTypeImpl[F[_]:Type,T:Type](f: Expr[T])(using qctx: QuoteContext): Expr[F[T]] = 
+  def checkPrintTypeImpl[F[_]:Type,T:Type](f: Expr[T])(using qctx: QuoteContext): Expr[Unit] = 
     import qctx.tasty.{_,given _}
 
-    @tailrec
     def uninline(t:Term):Term =
       t match
         case Inlined(_,_,x) => uninline(x)
         case _ => t
 
-    Expr.summon[CpsMonad[F]] match 
-      case Some(dm) =>
-           println(s"f.unseal=${f.unseal}")
-           val fu = uninline(f.unseal)
-           println(s"iuninlined =${fu}")
-           fu match 
-              case Block(_,Apply(TypeApply(Select(q,n),tparams),params)) =>
-                   println("apply, tparams=$tparams")
-                   '{ ${dm}.pure($f) }
-              case _ =>
-                   println("!apply")
-                    ???
-      case None => ???
-
-
-  def transformImpl[F[_]:Type,T:Type](f: Expr[T])(using qctx: QuoteContext): Expr[F[T]] = 
-    import qctx.tasty.{_,given _}
-    val flags = adoptFlags(f)
-    try
-      Expr.summon[CpsMonad[F]] match 
-        case Some(dm) => 
-             if (flags.printCode)
-                println(s"before transformed: ${f.show}")
-             if (flags.printTree)
-                println(s"value: ${f.unseal}")
-             val r = rootTransform[F,T](f,dm,flags,"",0).transformed
-             if (flags.printCode)
-                println(s"transformed value: ${r.show}")
-             if (flags.printTree)
-                println(s"transformed tree: ${r.unseal}")
-             r
-        case None => 
-             val ft = summon[quoted.Type[F]]
-             throw MacroError(s"Can't find async monad for ${ft.show}", f)
-    catch
-      case ex: MacroError =>
-           Reporting.error(ex.msg, ex.posExpr)
-           '{???}
-
-
-  def adoptFlags(f: Expr[_])(using qctx: QuoteContext): AsyncMacroFlags = 
-    import qctx.tasty.{_,given _}
-    Expr.summon[AsyncMacroFlags] match
-      case Some(flagsExpr) =>
-        flagsExpr match
-          case Unlifted(flags) => flags
-          case _  => 
-            throw MacroError(
-                    s"AsyncMacroFlags ($flagsExpr) is not a compile-time value", flagsExpr )
-      case None => 
-            import cps.macroFlags.{_, given _}
-            val printTree = Expr.summon[PrintTree.type].isDefined
-            val printCode = Expr.summon[PrintCode.type].isDefined
-            val debugLevel = Expr.summon[DebugLevel] match
-                 case Some(expr) =>
-                   expr match
-                      case Unlifted(v) => v.value
-                      case other  => 
-                          throw MacroError(s"DebugLevel ${other.show} is not a compile-time value", other)
-                 case None => 0
-            AsyncMacroFlags(printCode,printTree,debugLevel)
-  
-
-
-  def rootTransform[F[_]:Type,T:Type](f: Expr[T], dm:Expr[CpsMonad[F]], 
-                                      flags: AsyncMacroFlags,
-                                      exprMarker: String, nesting: Int)(
-                                           using qctx: QuoteContext): CpsExpr[F,T] =
-     val tType = summon[Type[T]]
-     import qctx.tasty.{_, given _}
-     import util._
-     val cpsCtx = TransformationContext[F,T](f,tType,dm,flags,exprMarker,nesting)
-     f match 
-         case Const(c) =>   ConstTransform(cpsCtx)
-         case '{ _root_.cps.await[F,$sType]($fs) } => 
-                            AwaitTransform(cpsCtx, sType, fs)
-         // looks like matching error in dotty.
-         // case '{ _root_.cps.await[$mType,$sType]($mst) } => 
-         //                    AwaitTransformOtherMonad(cpsCtx, mType, sType, mst.asInstanceOf[Expr[Any]])
-         case '{ if ($cond)  $ifTrue  else $ifFalse } =>
-                            IfTransform.run(cpsCtx, cond, ifTrue, ifFalse)
-         case '{ while ($cond) { $repeat }  } =>
-                            WhileTransform.run(cpsCtx, cond, repeat)
-         //case '{ try $body catch $cases finally $finalizer   } =>
-         //                  can't be determinated inside matching
-         case '{ throw $ex } =>
-                            ThrowTransform.run(cpsCtx, ex)
-         case _ => 
-             val fTree = f.unseal
-             fTree match {
-                case Apply(fun,args) =>
-                   ApplyTransform(cpsCtx).run(fun,args)
-                case TypeApply(fun,args) =>
-                   TypeApplyTransform(cpsCtx).run(fun,args)
-                case Assign(left,right) =>
-                   AssignTransform(cpsCtx).run(left,right)
-                case lambda@Lambda(params, body) =>
-                   LambdaTreeTransform.run(cpsCtx,lambda,params,body)
-                case Block(prevs,last) =>
-                   BlockTransform(cpsCtx).run(prevs,last)
-                case Ident(name) =>
-                   IdentTransform(cpsCtx).run(name)
-                case Typed(expr, tp) =>
-                   TypedTransform(cpsCtx).run(expr,tp)
-                case Try(body, cases, finalizer) =>
-                   TryTransform(cpsCtx).run(body,cases,finalizer)
-                case New(typeTree) =>
-                   NewTransform(cpsCtx).run(typeTree)
-                case thisTerm@This(qual) =>
-                   ThisTransform(cpsCtx).run(thisTerm)
-                case matchTerm@Match(scrutinee, caseDefs) =>
-                   MatchTreeTransform.run(cpsCtx, matchTerm)
-                case selectTerm: Select =>
-                   SelectTreeTransform.run(cpsCtx, selectTerm)
-                //   SelectOuter ? //TreeTransform.run(cpsCtx, selectTerm)
-                case inlinedTerm@ Inlined(call,bindings,body) =>
-                   InlinedTransform(cpsCtx).run(inlinedTerm)
-                case superTerm@Super(qual,mix) =>
-                   SuperTransform(cpsCtx).run(superTerm)
-                case returnTerm@Return(expr)=>
-                   ReturnTransform(cpsCtx).run(returnTerm)
-                case _ =>
-                   printf("fTree:"+fTree)
-                   throw MacroError(s"language construction is not supported: ${fTree}", f)
-             }
-     
-   
-  def nestTransform[F[_]:Type,T:Type,S:Type](f:Expr[S], 
-                                      cpsCtx: TransformationContext[F,T], 
-                                      marker:String)(using qctx:QuoteContext):CpsExpr[F,S]=
-        rootTransform(f,cpsCtx.monad,
-                      cpsCtx.flags,cpsCtx.exprMarker+marker,cpsCtx.nesting+1)
+    val fu = uninline(f.unseal)
+    fu match 
+              case Block(_,Apply(TypeApply(Select(q,n),tparams),List(param))) =>
+                   param.tpe match
+                      case AppliedType(tp,tparams1) =>
+                        val fType = summon[quoted.Type[F]]
+                        val ptp = tparams1.tail.head
+                        val ptpTree = Inferred(AppliedType(fType.unseal.tpe,List(ptp)))
+                        println("res:"+ptpTree.show)
+                      case None => 
+                        println(s"tpe=${param.tpe} is not AppliedType")
+                   '{ () }
+              case _ => ???
 
 
 }
--- src/test/scala/cps/TestCBS1ShiftIterableOps.scala
+++ src/test/scala/cps/TestCBS1ShiftIterableOps.scala
@@ -1,24 +1,7 @@
 package cps
 
-import org.junit.{Test,Ignore}
-import org.junit.Assert._
-
-import scala.quoted._
-import scala.util.Success
-import scala.util.Failure
-
-
-class TestBS1ShiftIterableOps:
-
-
-  @Test def testCollectFind(): Unit =
-     implicit val printCode = cps.macroFlags.PrintCode
-     //implicit val debugLevel = cps.macroFlags.DebugLevel(20)
-     val c = async1[ComputationBound]{
-          //val c = List(1,2,3,4)
-          //val r = c.collectFirst{ case x if x > 0 => x + await(T1.cbi(1)) > 3 }
-          //r
-          List(1,2,3,4).collectFirst{ case x if x > 0 => x + await(T1.cbi(1)) > 3 }
+     val c = async[ComputationBound]{
+          List(1,2,3,4).collectFirst{ case x if x > 0 => x > 3 }
      }
 
 
