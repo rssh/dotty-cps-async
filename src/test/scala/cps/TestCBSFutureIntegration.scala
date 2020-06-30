249c11e542ef28864c9aa110d70b331c6a7d8810
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2020-06-30 11:52:33 +0300

    added initial test

--- src/test/scala/cps/TestCBSFutureIntegration.scala
+++ src/test/scala/cps/TestCBSFutureIntegration.scala
@@ -3,11 +3,29 @@ package cps
 import org.junit.{Test,Ignore}
 import org.junit.Assert._
 
+import scala.concurrent._
+import scala.util._
+
+given future2ComputationBound[T](using ExecutionContext) as Conversion[Future[T],ComputationBound[T]] = 
+   future => 
+          ComputationBound.asyncCallback( listener => future.onComplete(listener) )
+   
 
 
 class TestCBSFutureIntegration:
 
-  def qqq: Unit = ???
+
+  @Test def futureBasic2(): Unit =
+     def fun(x:Int):Future[Int] =
+       import scala.concurrent.ExecutionContext.Implicits.global
+       Future successful (x+1)
+     val c = async[ComputationBound]{
+       //import scala.concurrent.ExecutionContext.Implicits.global
+       val a = await(fun(10))
+       a
+     }
+     assert(c.run()==Success(11))
+
 
 
 
