e9cd88cbc02890d70e5fa37c382719a49c98608f
Nicolas Stucki <nicolas.stucki@gmail.com>
2020-07-30 16:31:19 +0200

    Update Expr.betaReduce

--- src/main/scala/cps/forest/InlinedTransform.scala
+++ src/main/scala/cps/forest/InlinedTransform.scala
@@ -20,7 +20,7 @@ class InlinedTransform[F[_]:Type, T:Type](cpsCtx: TransformationContext[F,T]):
       nested
     else
       InlinedCpsExpr(using qctx)(monad, Seq(), inlinedTerm, nested)
-  
+
 
 class InlinedCpsExpr[F[_]:Type,T:Type](using qctx0: QuoteContext)(
                      monad: Expr[CpsMonad[F]],
@@ -33,8 +33,8 @@ class InlinedCpsExpr[F[_]:Type,T:Type](using qctx0: QuoteContext)(
    override def fLast(using qctx: QuoteContext): Expr[F[T]] =
       import qctx.tasty._
       val qctxOldInlined = oldInlined.asInstanceOf[qctx.tasty.Inlined]
-      val t = Inlined.copy(qctxOldInlined)(qctxOldInlined.call, 
-                               qctxOldInlined.bindings, 
+      val t = Inlined.copy(qctxOldInlined)(qctxOldInlined.call,
+                               qctxOldInlined.bindings,
                                nested.transformed.unseal)
       t.seal.asInstanceOf[Expr[F[T]]]
 
@@ -42,23 +42,22 @@ class InlinedCpsExpr[F[_]:Type,T:Type](using qctx0: QuoteContext)(
       new InlinedCpsExpr(using qctx0)(monad, exprs ++: prev, oldInlined, nested)
 
    override def append[A:Type](chunk: CpsExpr[F,A])(using QuoteContext): CpsExpr[F,A] =
-      if (nested.isAsync) 
+      if (nested.isAsync)
          InlinedCpsExpr(using qctx0)(monad, prev, oldInlined, nested.append(chunk))
-      else 
+      else
          chunk.prependExprs(Seq(StatementExprTreeGen(using qctx0)(oldInlined)))
 
    def syncOrigin(using QuoteContext): Option[Expr[T]] =
-      if (nested.isAsync) 
+      if (nested.isAsync)
         None
       else
         val expr = oldInlined.seal.asInstanceOf[Expr[T]]
         Some(expr)
 
    override def map[A:Type](f: Expr[T => A])(using QuoteContext): CpsExpr[F,A] =
-      syncOrigin match 
+      syncOrigin match
          case None => MappedCpsExpr(monad,Seq(),this,f)
-         case Some(origin) => CpsExpr.sync(monad, Expr.betaReduce(f)(origin))
-         
+         case Some(origin) => CpsExpr.sync(monad, Expr.betaReduce('{ $f($origin) }))
 
 }
 
