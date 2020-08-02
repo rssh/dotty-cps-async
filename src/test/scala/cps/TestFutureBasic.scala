2e5b1fc40866ee5b5bede3816ed9a9ab8f4105f5
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2020-08-02 12:33:31 +0300

    added test for fetch list

--- src/test/scala/cps/TestFutureBasic.scala
+++ src/test/scala/cps/TestFutureBasic.scala
@@ -9,6 +9,8 @@ import scala.language.postfixOps
 import scala.quoted._
 import scala.util.Success
 
+import java.util.Timer
+import java.util.TimerTask
 
 class TestFutureBasic:
 
@@ -36,10 +38,29 @@ class TestFutureBasic:
        s
      }
      val r = Await.result(c, 10 seconds)
-     println(s"r=$r")
      assert(r==(5050+100))
 
+  object FetchEmulator:
+ 
+    val timer = new Timer("fetchEmulator", true)
+
+    def retrieve(url: String, delay: Long):Future[String] = 
+      val p = Promise[String]()
+      timer.schedule(new TimerTask{
+          def run():Unit =
+             p.success("OK")
+        },
+        delay)
+      p.future
+    
 
 
+  @Test def futureFetchList(): Unit = 
+     val c = async[Future]{ 
+        val l = List("ürl1","url2", "url3")
+        l.map(FetchEmulator.retrieve(_,100)).map(await(_))
+     }
+     val r = Await.result(c, 10 seconds)
+     assert(r==List("OK","OK","OK"))
 
 
