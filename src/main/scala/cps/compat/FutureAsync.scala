7f520d5ba53d564f7efaa4f9526b94fa0165fed1
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2020-08-23 17:20:42 +0300

    imported tests from sip22-async

---
+++ src/main/scala/cps/compat/FutureAsync.scala
@@ -0,0 +1,20 @@
+package cps.compat
+
+import scala.concurrent._
+import cps._
+import cps.{ given CpsAsyncMonad[Future] }
+
+object FutureAsync:
+
+  inline def async[T](inline x:T)(using ec:ExecutionContext):Future[T]=
+  ${  
+    cps.Async.transformMonad('x, '{FutureAsyncMonad(using ec)} )
+  }
+
+  inline def await[T](x:Future[T])(using ec: ExecutionContext):T = 
+                       cps.await[Future, T](x)(using FutureAsyncMonad(using ec))
+
+
+val sip22 = FutureAsync
+
+
