7ebe7f83a041608df1fa6465a1d2a499d3264cef
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2020-08-30 13:00:03 +0300

    commented out yet-not-implemented stubs

--- src/main/scala/cps/runtime/FunctionAsyncShift.scala
+++ src/main/scala/cps/runtime/FunctionAsyncShift.scala
@@ -2,12 +2,19 @@ package cps.runtime
 
 import cps._
 
+// Nothing will work in current version (needs implement or inlining or shifted-lambda)
 class Function1AsyncShift[T1,R] extends AsyncShift[Function1[T1,R]]:
 
-   def andThen[F[_],A](f: Function[T1,R], m: CpsMonad[F])(g: (R) => F[A]): T1 => F[A] =
-        f.andThen(g)
+   def qqq = 1
 
-   def compose[F[_],A](f: Function[T1,R], m: CpsMonad[F])(g: A => F[T1]): A => F[R] =
-        x => m.map(g(x))(f)
+   //def andThen[F[_],A](f: Function[T1,R], m: CpsMonad[F])(g: (R) => F[A]): T1 => F[A] =
+   //    f.andThen(g)
+
+   // inline not aplied here for some reason.
+   //inline def andThen[F[_],A](f: Function[T1,R], m: CpsMonad[F])(g: (R) => F[A]): F[T1 => A] =
+   //       m.pure[T1=>A](a => await(g(f(a)))(using m))
+
+   //def compose[F[_],A](f: Function[T1,R], m: CpsMonad[F])(g: A => F[T1]): A => F[R] =
+   //     x => m.map(g(x))(f)
 
    
