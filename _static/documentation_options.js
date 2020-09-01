4dbcf614746e6f4ac76dd87fadeb0828bcbdf2ed
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2020-09-01 21:09:39 +0300

    updated site

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.0-RC1',
+    VERSION: '0.3.0-SNAPSHOT',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
