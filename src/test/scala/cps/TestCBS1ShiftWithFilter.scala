0b51fc311a706c4ea19335b9474d312687fe292f
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2020-09-26 12:59:36 +0300

    few test fro withFilter, trying to find source of compiler crash

--- src/test/scala/cps/TestCBS1ShiftWithFilter.scala
+++ src/test/scala/cps/TestCBS1ShiftWithFilter.scala
@@ -18,23 +18,58 @@ class TestCBS1ShiftWithFilter:
              } yield (x)
      }
      val r = c.run().get
-     println(s"withFilter: r==$r")
      assert(r(0) == 2 )
 
-/*
-  @Test def testSimple2: Unit =
+  @Test def testSimple1_map_shifted: Unit =
      implicit val printCode = cps.macroFlags.PrintCode
      //implicit val printTree = cps.macroFlags.PrintTree
      //implicit val debugLevel = cps.macroFlags.DebugLevel(20)
      val c = async[ComputationBound]{
           for{ x <- 1 to 10 if (x%2) == await(T1.cbi(0)) 
-               y <- 1 to 10 if (x%2) == await(T1.cbi(1)) 
-          } yield (x,y)
+             } yield (await(T1.cbi(x)))
      }
      val r = c.run().get
-     assert(r(0) == (0,1) )
+     assert(r(0) == 2 )
+
+/*
+//  dotty crash
+  @Test def testSimple2l: Unit =
+     implicit val printCode = cps.macroFlags.PrintCode
+     //implicit val printTree = cps.macroFlags.PrintTree
+     //implicit val debugLevel = cps.macroFlags.DebugLevel(20)
+     val c = async[ComputationBound]{
+          for{ x <- 1 to 10 if (x%2) == await(T1.cbi(0)) 
+               y <- 20 to 30 if (y%2) == await(T1.cbi(1)) 
+          } yield x+y
+     }
+     val r = c.run().get
+     assert(r(0) == 23 )
 */
 
 
+  @Test def testSimple2l2: Unit =
+     implicit val printCode = cps.macroFlags.PrintCode
+     //implicit val printTree = cps.macroFlags.PrintTree
+     //implicit val debugLevel = cps.macroFlags.DebugLevel(20)
+     val c = async[ComputationBound]{
+          for{ x <- 1 to 10 if (x%2) == await(T1.cbi(0)) 
+               y <- 20 to 30 
+          } yield (x,y)
+     }
+     val r = c.run().get
+     assert(r(0) == (2,20) )
+
+
+  @Test def testSimple2l2sync: Unit =
+     implicit val printCode = cps.macroFlags.PrintCode
+     //implicit val printTree = cps.macroFlags.PrintTree
+     //implicit val debugLevel = cps.macroFlags.DebugLevel(20)
+     val c = async[ComputationBound]{
+          for{ x <- 1 to 10 if (x%2) == await(T1.cbi(0)) 
+               y <- 20 to 30 if y%2 == 1
+          } yield (x,y)
+     }
+     val r = c.run().get
+     assert(r(0) == (2,21) )
 
 
