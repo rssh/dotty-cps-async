3b2b8196c771ff210ac401e90bb5ecb480e42861
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2020-10-04 18:51:46 +0300

    added example of callchain of async andThen

--- shared/src/test/scala/cps/TestCBS1ShiftFunction.scala
+++ shared/src/test/scala/cps/TestCBS1ShiftFunction.scala
@@ -20,8 +20,8 @@ class TestCBS1ShiftFunction:
      assert(c.run() == Success(6))
 
   @Test def testAndThen2(): Unit = 
-     implicit val printCode = cps.macroFlags.PrintCode
-     implicit val debugLevel = cps.macroFlags.DebugLevel(20)
+     //implicit val printCode = cps.macroFlags.PrintCode
+     //implicit val debugLevel = cps.macroFlags.DebugLevel(20)
      val c = async[ComputationBound]{
         def add1(x:Int):Int = x+1
          //  less ()
@@ -29,5 +29,14 @@ class TestCBS1ShiftFunction:
      }
      assert(c.run() == Success(7))
 
+  @Test def testAndThen3(): Unit = 
+     //implicit val printCode = cps.macroFlags.PrintCode
+     //implicit val debugLevel = cps.macroFlags.DebugLevel(20)
+     val c = async[ComputationBound]{
+        def add1(x:Int):Int = x+1
+         //  less ()
+        add1.andThen(x => x + await(T1.cbi(2))).andThen(x=>x+await(T1.cbi(1)))(3)
+     }
+     assert(c.run() == Success(7))
 
 
