917ca8f5b5682c04cafde63f90db42a55e0fca28
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2021-04-24 14:10:19 +0300

    add TestFizzBuzz for automatic coloring

---
+++ shared/src/test/scala/cps/pe/TestFizzBuzz.scala
@@ -0,0 +1,53 @@
+package cps.pe
+
+import org.junit.{Test,Ignore}
+import org.junit.Assert.*
+
+import scala.util.*
+
+import cps.*
+import cps.automaticColoring.{*,given}
+import cps.util.FutureCompleter
+import scala.language.implicitConversions
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class TestFizzBuzz:
+
+
+  //implicit val printCode: cps.macroFlags.PrintCode.type = cps.macroFlags.PrintCode
+  //implicit val printTree = cps.macroFlags.PrintTree
+  //implicit inline def debugLevel: cps.macroFlags.DebugLevel = cps.macroFlags.DebugLevel(10)
+
+
+  @Test def testFizBuzz: Unit = 
+     val c = async[PureEffect] {
+       val logger = PEToyLogger.make()
+       val counter = new PEIntRef(-1)
+       while {
+         val v = counter.increment()
+         logger.log(await(v).toString)
+         if (v % 3 == 0) then
+            logger.log("Fizz")
+         if (v % 5 == 0) then
+            logger.log("Buzz")
+         v < 10
+       } do ()
+       await(logger.all())
+     }
+     val future = c.unsafeRunFuture().map{ log =>
+       //println(s"PE:fizbuzz, log=${log} ")
+       assert(log(0)=="0")
+       assert(log(1)=="Fizz")
+       assert(log(2)=="Buzz")
+       assert(log(3)=="1")
+       assert(log(4)=="2")
+       assert(log(5)=="3")
+       assert(log(6)=="Fizz")
+       assert(log(7)=="4")
+     }
+     FutureCompleter(future)
+
+
+
+
