04f85a29f206611cd936e803a6a00410d033604f
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2021-07-09 16:48:35 +0300

    fix #10  (added tests)

--- shared/src/test/scala/cps/TestCBS1ShiftTryMonad.scala
+++ shared/src/test/scala/cps/TestCBS1ShiftTryMonad.scala
@@ -51,5 +51,36 @@ class TestCBS1ShiftTryMonad:
      assert(c.run() == Success(5))
 
 
+  @Test def testRestore(): Unit = 
+     val c = async[ComputationBound]{
+         val fa = T1.cbs("fa")
+         val q = summon[CpsTryMonad[ComputationBound]].restore(fa)(ex =>
+              T1.cbs(ex.toString + await(T1.cbs("!!!"))) 
+         )
+         await(q)
+     }
+     assert(c.run() == Success("fa"))
+
+  @Test def testRestoreNeg(): Unit = 
+     val c = async[ComputationBound]{
+         val fa  = Error[String](new RuntimeException("fa"))
+         val q = summon[CpsTryMonad[ComputationBound]].restore(fa)(ex =>
+              T1.cbs(ex.toString + await(T1.cbs("!!!"))) 
+         )
+         await(q)
+     }
+     val Success(v) = c.run()
+     assert(v.endsWith("!!!"))
+
+  @Test def testWithAction(): Unit = 
+     val c = async[ComputationBound]{
+         var x = 0 
+         val fa = T1.cbs("fa")
+         val q = summon[CpsTryMonad[ComputationBound]].withAction(fa)( {x = await(T1.cbi(1))} )
+         await(q)
+         x
+     }
+     assert(c.run() == Success(1))
+
 
 
