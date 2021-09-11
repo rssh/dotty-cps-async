e02f511f34e880f483890460da65d493c736a9c5
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2021-09-11 09:48:31 +0300

    better scaladoc for CpsMonadConversion

--- shared/src/main/scala/cps/CpsMonadConversion.scala
+++ shared/src/main/scala/cps/CpsMonadConversion.scala
@@ -1,5 +1,10 @@
 package cps
 
+/**
+ * CpsMonadConversion -- conversion from `F[_]` to `G[_]`.
+ *  If the given instance of such morphism exists, then `await[F]` can be used inside `async[G]`
+ *@see [[cps.await]], [[cps.async]], [[cps.CpsMonad]]
+ **/
 trait CpsMonadConversion[F[_],G[_]]:
   def apply[T](ft:F[T]): G[T]
 
