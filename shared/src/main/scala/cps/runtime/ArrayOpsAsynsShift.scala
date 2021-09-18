a8b16a90592207b5883f76de22b3476f9810f64a
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2021-09-18 22:31:23 +0300

    #26 - implementing ArrayOps high-order functions in progress

--- shared/src/main/scala/cps/runtime/ArrayOpsAsynsShift.scala
+++ shared/src/main/scala/cps/runtime/ArrayOpsAsynsShift.scala
@@ -1,14 +1,16 @@
 package cps.runtime
 
 import cps._
+import scala.annotation.targetName
 import scala.collection._
 import scala.reflect.ClassTag
 import scala.collection.mutable.ArrayBuilder
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.immutable.Map
 import java.util.concurrent.atomic.AtomicReference
 
 class ArrayOpsAsyncShift[A] extends AsyncShift[ArrayOps[A]] {
 
-  // TODO: think about default semantics for foreach.
   def foreachParallel[F[_],U](arrayOps: ArrayOps[A], monad: CpsMonad[F])(f: A => F[U]): F[Unit] = {
      val r = new AtomicReference[F[Unit]](monad.pure(()))
      arrayOps.foreach{ a =>
@@ -32,7 +34,7 @@ class ArrayOpsAsyncShift[A] extends AsyncShift[ArrayOps[A]] {
       foreachSequential(arrayOps, monad)(f)  
   }
 
-  def map[F[_]:CpsMonad,B:ClassTag](arr: ArrayOps[A], monad: CpsMonad[F])(f: A=> F[B]):F[Array[B]] = {
+  def map[F[_],B](arr: ArrayOps[A], monad: CpsMonad[F])(f: A=> F[B])(using ct:ClassTag[B]):F[Array[B]] = {
      val mappedArr = arr.map(f)
      val r = new Array[B](arr.knownSize)
      val fu = mappedArr.zipWithIndex.foldLeft(monad.pure(())){
@@ -42,7 +44,7 @@ class ArrayOpsAsyncShift[A] extends AsyncShift[ArrayOps[A]] {
      monad.map(fu)(_ => r)
   }
 
-  def flatMap[F[_]:CpsMonad,B:ClassTag](arr: ArrayOps[A], monad: CpsMonad[F])(f: A=> F[IterableOnce[B]]):F[Array[B]] = {
+  def flatMap[F[_],B](arr: ArrayOps[A], monad: CpsMonad[F])(f: A=> F[IterableOnce[B]])(using ct:ClassTag[B]):F[Array[B]] = {
      val b = monad.pure(ArrayBuilder.make[B])
      monad.map(
       arr.foldLeft(b)((s,e) => 
@@ -54,5 +56,133 @@ class ArrayOpsAsyncShift[A] extends AsyncShift[ArrayOps[A]] {
       )
      )( _.result )
   }
+
+  def flatMap[F[_],BS,B](arr: ArrayOps[A], monad: CpsMonad[F])(f: A=> F[BS])(using asIterable:BS=>Iterable[B], ct:ClassTag[B]):F[Array[B]] = {
+     flatMap(arr,monad)(a => monad.map(f(a))(bs => asIterable(bs)))
+  }
+
+  def fold[F[_],A1 >: A](arrOps: ArrayOps[A], monad: CpsMonad[F])(z: A1)(op:(A1,A1)=>F[A1]):F[A1] = {
+     arrOps.foldLeft[F[A1]](monad.pure(z)){ (s,e) =>
+        monad.flatMap(s){ s1 => op(s1,e) }
+     }
+  }
+
+
+  def collectFirst[F[_],B](arrOps: ArrayOps[A], monad: CpsMonad[F])(f: PartialFunction[A,F[B]]): F[Option[B]] = {
+     arrOps.collectFirst(f) match
+        case Some(fb) => monad.map(fb)(x => Some(x))
+        case None => monad.pure(None)
+  }
+
+  def collect[F[_],B](arrOps: ArrayOps[A], monad: CpsMonad[F])(f: PartialFunction[A,F[B]])(using ClassTag[B]): F[Array[B]] = {
+     val build = arrOps.collect(f).foldLeft(monad.pure(ArrayBuilder.make[B])){ (s,e) =>
+         monad.flatMap(s){ s1 =>
+          monad.map(e){ e1 =>
+           s1.addOne(e1)
+           s1
+         }
+        } 
+     }
+     monad.map(build)(_.result)
+  }
+
+
+  def count[F[_]](arr: ArrayOps[A], monad: CpsMonad[F])(p: A=> F[Boolean]): F[Int] = {
+     arr.foldLeft[F[Int]](monad.pure(0)){ (s,e) =>
+        monad.flatMap(s){v =>
+          monad.map(p(e)){ x =>
+            if x then v+1 else v
+          }
+        }
+     }
+  }
+
+  def dropWhile[F[_]](arr: ArrayOps[A], monad: CpsMonad[F])(p: A=> F[Boolean]): F[Array[A]] = {
+     def skipWhile(it: Iterator[A], i:Int):F[Array[A]] =
+       if !it.hasNext then
+          monad.pure(arr.slice(0,1))
+       else
+          val e = it.next
+          monad.flatMap(p(e)){ v =>
+             if (v) then skipWhile(it,i+1) else monad.pure(arr.slice(i,arr.size))
+          }
+     skipWhile(arr.iterator,0)
+  }
+
+  def exists[F[_]](arr: ArrayOps[A], monad: CpsMonad[F])(p: A=> F[Boolean]): F[Boolean] = {
+     def steps(it: Iterator[A]):F[Boolean] =
+       if !it.hasNext then
+          monad.pure(false)
+       else
+          val e = it.next
+          monad.flatMap(p(e)){ v =>
+             if (v) then monad.pure(true) else steps(it)
+          }
+     steps(arr.iterator)
+  }
+
+  def find[F[_]](arrOps: ArrayOps[A], monad: CpsMonad[F])(p: A=> F[Boolean]): F[Option[A]] = {
+     def steps(it: Iterator[A]):F[Option[A]] =
+       if !it.hasNext then
+          monad.pure(None)
+       else
+          val e = it.next
+          monad.flatMap(p(e)){ v =>
+             if (v) then monad.pure(Some(e)) else steps(it)
+          }
+     steps(arrOps.iterator)
+  }
+
+  def filter[F[_]](arrOps: ArrayOps[A], monad: CpsMonad[F])(p: A=> F[Boolean]): F[Array[A]] = {
+     val tmp = arrOps.slice(0,arrOps.size)
+     def steps(it: Iterator[A], i:Int):F[Array[A]] =
+       if !it.hasNext then
+          monad.pure(tmp.slice(0,i))
+       else
+          val e = it.next
+          monad.flatMap(p(e)){ v =>
+             if v then 
+               tmp(i) = e
+               steps(it,i+1)
+             else
+               steps(it,i)
+          }
+     steps(arrOps.iterator, 0)
+  }
+
+  def filterNot[F[_]](arrOps: ArrayOps[A], monad: CpsMonad[F])(p: A=> F[Boolean]): F[Array[A]] = {
+        filter[F](arrOps, monad)(a => monad.map(p(a))(! _))
+  }
+
+  def groupBy[F[_],K](arrOps: ArrayOps[A], monad: CpsMonad[F])(f: A=> F[K]): F[Map[K,Array[A]]] = {
+        def step(it: Iterator[A], gathered: Map[K,ArrayBuffer[A]]):F[Map[K,ArrayBuffer[A]]] =
+          if !it.hasNext then
+             monad.pure(gathered)
+          else
+             val e = it.next
+             monad.flatMap(f(e)){ k =>
+               gathered.get(k) match
+                 case Some(r) => r.addOne(e)
+                                 step(it, gathered)
+                 case None =>
+                    val r = ArrayBuffer(e)
+                    step(it, gathered.updated(k,r))
+             }
+        val s: F[Map[K,ArrayBuffer[A]]] = step(arrOps.iterator, Map.empty) 
+        monad.map(s){ (gathered: Map[K,ArrayBuffer[A]]) =>
+          gathered.view.mapValues{ v =>
+             val l = v.length
+             // no element ClassTag, for using toArray
+             val r = arrOps.slice(0,l)
+             var i = 0
+             while(i < l) {
+               r(i) = v(i) 
+               i = i + 1
+             }
+             r
+          }.toMap
+        }
+  }
+
 
 }
---
+++ shared/src/test/scala/cps/TestCBS1ShiftArrayOps.scala
@@ -0,0 +1,449 @@
+package cps
+
+import org.junit.{Test,Ignore}
+import org.junit.Assert._
+
+import scala.quoted._
+import scala.util.Success
+import scala.util.Failure
+
+
+class TestCBS1ShiftArrayOps:
+
+
+  @Test def testMap(): Unit = 
+     //implicit val printCode = cps.macros.flags.PrintCode
+     //implicit val debugLevel = cps.macros.flags.DebugLevel(20)
+     val c = async[ComputationBound]{
+        Array(1,2,3).map{ x =>
+           await(T1.cbi(3)) + x
+        }
+     }
+     val r = c.run()
+     val v = r.get
+     assert(v(0)==4)
+     assert(v(1)==5)
+     assert(v(2)==6)
+
+  @Test def testMapVal(): Unit = 
+     val c = async[ComputationBound]{
+        val arr = Array(1,2,3)
+        arr.map{ x =>
+           await(T1.cbi(3)) + x
+        }
+     }
+     val r = c.run()
+     val v = r.get
+     assert(v(0)==4)
+     assert(v(1)==5)
+     assert(v(2)==6)
+
+  @Test def testForeachArray(): Unit = 
+     val c = async[ComputationBound]{
+        var s = 0
+        Array(1,2,3).foreach{ x =>
+           s += await(T1.cbi(3)) + x
+        }
+        s
+     }
+     assert(c.run() == Success(15))
+
+  
+  @Test def testCount(): Unit =
+     val c = async[ComputationBound]{
+               Array(T1.cbi(1),T1.cbi(2),T1.cbi(3)).count(x => await(x)%2==1)
+             }
+     assert(c.run() == Success(2))
+
+  //TODO: @Test def testDistinctBy(): Unit = ???
+
+  @Test def testDropWhile(): Unit =
+     val c = async[ComputationBound]{
+               Array(T1.cbi(1),T1.cbi(2),T1.cbi(3)).dropWhile(x => await(x) < 3)
+             }
+     c.run() match {
+        case Success(v) =>
+            assert(v(0).run() == Success(3))
+        case Failure(ex) =>
+            assert(false,"dropWhile result should be successed")
+     }
+
+  @Test def testExistsT(): Unit =
+     val c = async[ComputationBound]{
+                Array(T1.cbi(1),T1.cbi(2),T1.cbi(3)).exists(x => await(x)==2)
+     }
+     assert(c.run() == Success(true))
+
+  @Test def testExistsF(): Unit =
+     val c = async[ComputationBound]{
+                Array(T1.cbi(1),T1.cbi(2),T1.cbi(3)).exists(x => await(x)==1000)
+     }
+     assert(c.run() == Success(false))
+
+  @Test def testFindT(): Unit =
+     val c = async[ComputationBound]{
+                Array(T1.cbi(1),T1.cbi(2),T1.cbi(3)).find{x => await(x)==3 }
+     }
+     val l = c.run()
+     l match {
+        case Success(Some(v)) =>
+            assert(v.run() == Success(3))
+        case Success(None) =>
+            assert(false,"find result should be not None")
+        case Failure(ex) =>
+            assert(false,"find result should be successed")
+     }
+
+  @Test def testFindF(): Unit =
+     val c = async[ComputationBound]{
+                Array(T1.cbi(1),T1.cbi(2),T1.cbi(3)).find( x => await(x)==1000 )
+     }
+     val l = c.run()
+     assert(l == Success(None)) 
+
+  @Test def testFilter(): Unit =
+     val c = async[ComputationBound]{
+              Array(1,2,3,4,5,6,7,8,9,10).filter( x => x % 3 == await(T1.cbi(0)) )
+     }
+     val r = c.run().get
+     assert(r(0)==3)
+     assert(r(1)==6)
+     assert(r(2)==9)
+     
+  //TODO: determinate
+  //@Test def testFlatMap(): Unit =
+  //   implicit val printCode = cps.macros.flags.PrintCode
+  //   val c = async[ComputationBound]{
+  //       val bb = Array(Array(1,1,3),Array(1,2,3),Array(1,3,4))
+  //       val aa = Array(0,1,2)
+  //       // chains not work
+  //       //val s = for{ a <- aa
+  //       //     b <- bb(a)
+  //       //} yield a+b+await(T1.cbi(0))
+  //       //val s = aa.flatMap(a => bb(a).map(b => a+b+await(T1.cbi(0))))
+  //       val s = aa.flatMap(a => bb(await(T1.cbi(a))))
+  //       s
+  //   }
+  //   val r = c.run().get
+  //   assert(r(0)==1)
+  //   assert(r(1)==3)
+  //   assert(r(2)==5)
+
+  @Test def testFold1(): Unit =
+     val c = async[ComputationBound]{
+          Array(1,2,3).fold(0)(
+                   (x:Int,y:Int) => x + y + await(T1.cbi(1)) 
+          )
+     }
+     val l = c.run()
+     assert(l == Success(9)) 
+
+  @Test def testCollectFirst(): Unit =
+     //implicit val printCode = cps.macroFlags.PrintCode
+     //implicit val printTree = cps.macroFlags.PrintTree
+     //implicit val debugLevel = cps.macroFlags.DebugLevel(20)
+     val c = async[ComputationBound]{
+          val c = Array(1,2,3,4)
+          val r: Option[Int] = c.collectFirst{ case x if x > 2 => x + await(T1.cbi(1)) }
+          //val r: Option[Int] = await(T1.cbt(Some(4)))
+          r
+     }
+     assert(c.run() == Success(Some(4)))
+
+  @Test def testCollect(): Unit =
+     //implicit val printCode = cps.macroFlags.PrintCode
+     //implicit val debugLevel = cps.macroFlags.DebugLevel(20)
+     val c = async[ComputationBound]{
+          val c = Array(1,2,3,4)
+          c.collect{ case x if x % 2 == 0 => x + await(T1.cbi(1)) }
+     }
+     val r = c.run().get
+     assert(r(0)==3)
+     assert(r(1)==5)
+
+
+  @Test def testGroupByEmpty(): Unit =
+     //implicit val printCode = cps.macroFlags.PrintCode
+     //implicit val printTree = cps.macroFlags.PrintTree
+     //implicit val debugLevel = cps.macroFlags.DebugLevel(20)
+     val c = async[ComputationBound]{
+          val c:Array[String] = Array()
+          c.groupBy(x => await(T1.cbi(x.length)))
+     }
+     assert(c.run() == Success(Map.empty[Int,String]))
+
+  @Test def testGroupBy(): Unit =
+     //implicit val printCode = cps.macroFlags.PrintCode
+     //implicit val printTree = cps.macroFlags.PrintTree
+     //implicit val debugLevel = cps.macroFlags.DebugLevel(20)
+     val c = async[ComputationBound]{
+          val c:Array[String] = Array("","a","aa","bb","aaa","bbb","ccc")
+          c.groupBy(x => await(T1.cbi(x.length)))
+     }
+     val r = c.run().get
+     assert(r(0).toList.size == 1)
+     assert(r(1).toList.size == 1)
+     assert(r(2).toList.size == 2)
+     assert(r(3).toList.size == 3)
+
+
+  /*
+  @Test def testCorrespondsSame(): Unit =
+     //implicit val printCode = cps.macroFlags.PrintCode
+     val c = async[ComputationBound]{
+         val l1 = List(1,2,3)
+         val l2 = l1
+         l1.corresponds(l2)((a,b) => await(T1.cbt(a == b)) )
+     }
+     assert(c.run() == Success(true))
+
+  @Test def testCorrespondsNoSame(): Unit =
+     //implicit val printCode = cps.macroFlags.PrintCode
+     val c = async[ComputationBound]{
+         val l1 = List(1,2,3)
+         val l2 = List(1,4,1)
+         l1.corresponds(l2)((a,b) => await(T1.cbt(a == b)) )
+     }
+     assert(c.run() == Success(false))
+
+  @Test def testCorrespondsDiffLen1(): Unit =
+     //implicit val printCode = cps.macroFlags.PrintCode
+     val c = async[ComputationBound]{
+         val l1 = List(1,2,3)
+         val l2 = List(1,2,3,3)
+         l1.corresponds(l2)((a,b) => await(T1.cbt(a == b)) )
+     }
+     assert(c.run() == Success(false))
+
+  @Test def testCorrespondsDiffLen2(): Unit =
+     //implicit val printCode = cps.macroFlags.PrintCode
+     val c = async[ComputationBound]{
+         val l1 = List(1,2,3,3)
+         val l2 = List(1,2,3)
+         l1.corresponds(l2)((a,b) => await(T1.cbt(a == b)) )
+     }
+     assert(c.run() == Success(false))
+
+
+  @Test def testFoldLeft(): Unit =
+     val c = async[ComputationBound]{
+          Seq(1,2,3).foldLeft("")((s,e) => "(" + s + "," + await(T1.cbi(e)).toString + ")" )
+     }
+     assert(c.run() == Success("(((,1),2),3)"))
+
+  @Test def testFoldRight(): Unit =
+     val c = async[ComputationBound]{
+          Seq(1,2,3).foldRight("")((e,s) => "(" + await(T1.cbs(e.toString)) + "," + s + ")" )
+     }
+     assert(c.run() == Success("(1,(2,(3,)))"))
+
+
+  @Test def forall1(): Unit =
+     val c = async[ComputationBound]{
+          Seq(1,2,3).forall(_ == await(T1.cbi(3)))
+     }
+     assert(c.run() == Success(false))
+
+  @Test def forall2(): Unit =
+     val c = async[ComputationBound]{
+          Seq(1,2,3).forall(_ > await(T1.cbi(0)))
+     }
+     assert(c.run() == Success(true))
+
+  @Test def forall3(): Unit =
+     val c = async[ComputationBound]{
+          Seq().forall(_ == await(T1.cbi(0)))
+     }
+     assert(c.run() == Success(true))
+
+  @Test def testGroupMap1(): Unit =
+     //implicit val printCode = cps.macroFlags.PrintCode
+     //implicit val printTree = cps.macroFlags.PrintTree
+     //implicit val debugLevel = cps.macroFlags.DebugLevel(20)
+     val c = async[ComputationBound]{
+          val c:List[String] = List("","a","aa","bb","aaa","bbb","ccc")
+          c.groupMap(x => await(T1.cbi(x.length)))(identity)
+     }
+     val r = c.run().get
+     assert(r(0).toList.size == 1)
+     assert(r(1).toList.size == 1)
+     assert(r(2).toList.size == 2)
+     assert(r(3).toList.size == 3)
+
+  @Test def testGroupMap2(): Unit =
+     //implicit val printCode = cps.macroFlags.PrintCode
+     //implicit val printTree = cps.macroFlags.PrintTree
+     //implicit val debugLevel = cps.macroFlags.DebugLevel(20)
+     val c = async[ComputationBound]{
+          val c:List[String] = List("","a","aa","bb","aaa","bbb","ccc")
+          c.groupMap(x => await(T1.cbi(x.length)))(x => await(T1.cbs(x+"1")))
+     }
+     val r = c.run().get
+     assert(r(0).toList.size == 1)
+     assert(r(0)(0) == "1")
+     assert(r(1).toList.size == 1)
+     assert(r(1)(0) == "a1")
+     assert(r(2).toList.size == 2)
+     assert(r(3).toList.size == 3)
+
+
+  @Test def testGroupMapReduce(): Unit =
+     //implicit val printCode = cps.macroFlags.PrintCode
+     val c = async[ComputationBound]{
+          val c:List[String] = List("","a","aa","bb","aaa","bbb","ccc")
+          c.groupMapReduce(x => await(T1.cbi(x.length)))(x => await(T1.cbs(x+"1")))( _ + _ )
+     }
+     val r = c.run().get
+     assert(r(0) == "1")
+     assert(r(1) == "a1")
+     assert(r(2) == "aa1bb1")
+     assert(r(3) == "aaa1bbb1ccc1")
+
+  @Test def testGroupMapReduce1(): Unit =
+     //implicit val printCode = cps.macroFlags.PrintCode
+     val c = async[ComputationBound]{
+          val c:List[String] = List("","a","aa","bb","aaa","bbb","ccc")
+          c.groupMapReduce(x => await(T1.cbi(x.length)))(x => await(T1.cbs(x+"1")))( (w:String,v:String) => w + await(T1.cbs(v)) )
+     }
+     val r = c.run().get
+     assert(r(0) == "1")
+     assert(r(1) == "a1")
+     assert(r(2) == "aa1bb1")
+     assert(r(3) == "aaa1bbb1ccc1")
+
+  @Test def testMaxBy(): Unit =
+     val c = async[ComputationBound]{
+          val x: Seq[Int] = (1 to 100).toSeq 
+          x maxBy ( _ % await(T1.cbi(77)) )
+     }
+     val r = c.run().get
+     assert(r == 76)
+
+  @Test def testMinBy(): Unit =
+     val c = async[ComputationBound]{
+          val x: Seq[Int] = (1 to 100).toSeq 
+          x minBy ( _ % await(T1.cbi(77)) )
+     }
+     val r = c.run().get
+     assert(r == 77)
+
+  @Test def testScanLeft(): Unit =
+     val c = async[ComputationBound]{
+          val l = (1 to 5).toSeq
+          l.scanLeft(0)( (x,y) => x + await(T1.cbi(y)) )
+     }
+     val r = c.run().get
+     assert(r == Seq(0,1,3,6,10,15))
+
+  @Test def testScanRight(): Unit =
+     val c = async[ComputationBound]{
+          val l = (1 to 5).toSeq
+          l.scanRight(0)( (x,y) => x + await(T1.cbi(y)) )
+     }
+     val r = c.run().get
+     assert(r == Seq(0,5,9,12,14,15))
+
+
+  @Test def testTakeWhile(): Unit =
+     val c = async[ComputationBound]{
+        val l = Vector(1,2,3,4,5,6,7,8,3,1)
+        l.takeWhile( _ < await(T1.cbi(5)) )
+     }
+     val r = c.run().get
+     assert(r == Vector(1,2,3,4))
+
+  @Test def testSpan(): Unit =
+     val c = async[ComputationBound]{
+        val l = Vector(1,2,3,4,5,6,7,8,3,1)
+        l.span( _ < await(T1.cbi(5)) )
+     }
+     val r = c.run().get
+     assert(r._1 == Vector(1,2,3,4))
+     assert(r._2 == Vector(5,6,7,8,3,1))
+
+  @Test def testPartition(): Unit =
+     val c = async[ComputationBound]{
+        val l = Vector(1,2,3,4,5,6,7,8,3,1)
+        l.partition( _ < await(T1.cbi(5)) )
+     }
+     val r = c.run().get
+     assert(r._1 == Vector(1,2,3,4,3,1))
+     assert(r._2 == Vector(5,6,7,8))
+
+  @Test def testPartitionMap(): Unit =
+     val c = async[ComputationBound]{
+        val l = Vector(1,2,3,4,5,6,7,8,3,1)
+        l.partitionMap( x => if (x < await(T1.cbi(5))) Left(x) else Right(x.toString) )
+     }
+     val r = c.run().get
+     assert(r._1 == Vector(1,2,3,4,3,1))
+     assert(r._2 == Vector("5","6","7","8"))
+
+  @Test def testReduce1(): Unit =
+     val c = async[ComputationBound]{
+        val l = 1 to 10
+        l.reduce((x,y) => await(T1.cbi(x+y)))
+     }
+     val r = c.run().get
+     assert(r == (1 to 10).sum )
+
+  @Test def testReduceEmpty(): Unit =
+     val c = async[ComputationBound]{
+        val l = List.empty[Int]
+        l.reduce((x,y) => await(T1.cbi(x+y)))
+     }
+     assert( c.run().isFailure )
+
+  @Test def testReduceLeft(): Unit =
+     val c = async[ComputationBound]{
+        val l = (0 to 9).map(_.toString)
+        l.reduceLeft((x,y) => await(T1.cbs("("+x+y+")")))
+     }
+     val r = c.run().get
+     assert(r == "(((((((((01)2)3)4)5)6)7)8)9)" )
+
+  @Test def testReduceLeftEmpty(): Unit =
+     val c = async[ComputationBound]{
+        val l = List.empty[String]
+        l.reduceLeft((x,y) => await(T1.cbs("("+x+y+")")))
+     }
+     val r = c.run()
+     assert(r.isFailure)
+
+  @Test def testReduceRight(): Unit =
+     val c = async[ComputationBound]{
+        val l = (0 to 9).map(_.toString)
+        l.reduceRight((x,y) => await(T1.cbs("("+x+y+")")))
+     }
+     val r = c.run().get
+     assert(r == "(0(1(2(3(4(5(6(7(89)))))))))" )
+
+  @Test def testReduceRightEmpty(): Unit =
+     val c = async[ComputationBound]{
+        val l = List.empty[String]
+        l.reduceRight((x,y) => await(T1.cbs("("+x+y+")")))
+     }
+     val r = c.run()
+     assert(r.isFailure)
+
+  @Test def testTapEach(): Unit =
+     class X(val value:Int, var tapped:Boolean)
+     val v = (1 to 10).map( new X(_, false) )
+     val c = async[ComputationBound]{
+        v.tapEach( x => { await(T1.cbi(0)); x.tapped = true  })
+     }
+     val r = c.run()
+     assert(v.forall(_.tapped))
+
+  @Test def testWithFilterSmoke(): Unit =
+     //implicit val printCode = cps.macroFlags.PrintCode
+     //implicit val debugLevel = cps.macroFlags.DebugLevel(20)
+     val c = async[ComputationBound]{
+        (0 to 9).withFilter(x => await(T1.cbi(0)) == x%2).map(_.toString) 
+     }
+     val r = c.run()
+     assert(r == Success(IndexedSeq("0","2","4","6","8")) )
+  */
+
+
+
