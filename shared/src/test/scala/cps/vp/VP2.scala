87ffa91f9ea5ce6f89c2294148ef4d9d5da56488
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2021-11-22 20:28:53 +0200

    ensure of gragment compilation, which was failed in previous version

---
+++ shared/src/test/scala/cps/vp/VP2.scala
@@ -0,0 +1,45 @@
+package cps.vp
+
+import cps.*
+import cps.monads.{*,given}
+
+import scala.concurrent.*
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.control.NonFatal
+
+object VP2:
+
+  case class TestContext(clientId: String, url: String, trace: Boolean = false)
+
+
+  class Test {
+    def run(ctx:TestContext): Future[Unit] = ???
+  }
+
+  val ExtremeSize = new Test
+  def tests: Seq[Test] = ???
+
+  def run1(url: String, clientId: String): Future[Unit] = async[Future] {  
+    val testContext = TestContext(clientId, url)
+    for(test <- tests) {
+      println(s"running ${test}")
+      try {
+        await(test.run(testContext))
+      }catch{
+        case NonFatal(ex) =>
+          if !(test eq ExtremeSize) then
+            println(s"exception during test: ${ex.getMessage} rerunning with trace")
+            val traceContext = testContext.copy(trace=true)
+            try {
+              await(test.run(traceContext))
+            } catch {
+              case NonFatal(ex) =>
+            }
+          else
+            println(s"exception during test ${test}, ${ex.getMessage}")
+      }
+    }
+    println(s"summary metrics: ${testContext}")
+  }
+
+
