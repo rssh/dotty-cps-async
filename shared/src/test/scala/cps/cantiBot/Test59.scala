7a95b5e3b0965e7e04c3b761ea9b5560aa35fb0d
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2022-07-07 10:33:55 +0300

    testcase for #59

---
+++ shared/src/test/scala/cps/cantiBot/Test59.scala
@@ -0,0 +1,21 @@
+package cps.cantiBot
+
+import cps.*
+import cps.monads.FutureAsyncMonad
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class TestCase:
+  val objs = Seq(
+    new AnyRef:
+      def curry(v: Any)(w: Any): Unit = ()
+
+      def bad_func(): Future[Unit] =
+        async[Future] {
+          for message <- Nil.toSeq do
+            curry(Nil.toSeq)
+          ()
+        }
+  )
+
