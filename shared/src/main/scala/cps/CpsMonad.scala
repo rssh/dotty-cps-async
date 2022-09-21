716c74b136e39b1ed38f81cda888f790537dbea3
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2022-09-21 23:28:22 +0300

    moved CpsAwaitbale from CpsMonad

--- shared/src/main/scala/cps/CpsMonad.scala
+++ shared/src/main/scala/cps/CpsMonad.scala
@@ -64,13 +64,6 @@ object CpsMonad {
 }
 
 
-/**
- * Marker typeclass for wrappers, which we can await.
- * Such traits can be not monads itself (for example, its impossible to set monad structure over js.Promise)
- * but can be convertable into cps monads.
- **/
-trait CpsAwaitable[F[_]] 
-
 
 /**
  * If you monad supports this typeclass, than
