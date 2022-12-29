743f89012e03c024b6b9dd64f12165d8f5555552
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2022-12-29 10:35:03 +0200

    changed release

--- shared/src/main/scala/cps/Async.scala
+++ shared/src/main/scala/cps/Async.scala
@@ -36,9 +36,9 @@ transparent inline def async[F[_]](using am: CpsMonad[F]) =
 transparent inline def reify[F[_]](using am: CpsMonad[F]) =
    macros.Async.InferAsyncArg(using am)
 
-///**
-// * Synonym for `await` 
-// **/
-//transparent inline def reflect[F[_],T,G[_]](f: F[T])(using inline am:CpsAwaitable[F], inline ctx: CpsMonadContext[G]): T = 
-//   await[F,T,G](f)
+/**
+ * Synonym for `await` 
+ **/
+transparent inline def reflect[F[_],T,G[_]](f: F[T])(using inline am:CpsAwaitable[F], inline ctx: CpsMonadContext[G]): T = 
+   await[F,T,G](f)
 
