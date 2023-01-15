456893cb757dd0bb6c5930f9ad48f8536b810385
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2023-01-15 15:15:33 +0200

    more tests for #65

--- shared/src/test/scala/cps/pe/TestPEAsyncIterator.scala
+++ shared/src/test/scala/cps/pe/TestPEAsyncIterator.scala
@@ -16,37 +16,28 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 class TestPEAsyncIterator {
 
-  val N = 10_000_000
+  val N = 10_000
 
   @Test 
-  @Ignore
-  def runIteratorOnAsyncList() = {
+  def peRunIteratorOnAsyncList() = {
 
     val stream: AsyncList[PureEffect, Int] = asyncStream[AsyncList[PureEffect, Int]] { out =>
-      println("begin generator")
       out.emit(0)
-      println("plop")
       for i <- 1 to N do out.emit(i)
     }
 
-    println("before for loop")
     val ite = stream.iterator
     val compute: PureEffect[Long] = async[PureEffect] {
-      println("begin sink, before calling ite.next")
       var n = await(ite.next)
-      println(s"after ite.next, n=$n")
       var res: Long = 0
-      println("before while")
       var count: Int = 0
       while (n.nonEmpty) {
-        res += n.get
+        val v = n.get
+        res += v
         count += 1
-        println(s"receive res $res")
         n = await(ite.next)
       }
-
-      println(s"pe.asuncList:count=$count")
-      assert(count == N)
+      assert(count == N+1)
       res
     }
 
