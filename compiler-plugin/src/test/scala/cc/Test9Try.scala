8cfdac8a0b2e27fe020f9c0f24e623d36d174203
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2023-06-03 09:34:02 +0300

    more tests for try in the compiler plugin

--- compiler-plugin/src/test/scala/cc/Test9Try.scala
+++ compiler-plugin/src/test/scala/cc/Test9Try.scala
@@ -8,7 +8,8 @@ class Test9Try {
 
   @Test
   def testCompileAndRun(): Unit = {
-    DotcInvocations.checkRuns(selection = ".*m6_000".r)(
+    DotcInvocations.checkRuns(selection = (".*" +
+      "").r)(
       TestRun("testdata/set9Try/m1", "cpstest.Test9m1", "Right(10)\n"),
       TestRun("testdata/set9Try/m2", "cpstest.Test9m2"),
       TestRun("testdata/set9Try/m3", "cpstest.Test9m3"),
@@ -19,8 +20,8 @@ class Test9Try {
       TestRun("testdata/set9Try/m5_2", "cpstest.Test9m5_2"),
       TestRun("testdata/set9Try/m5_2e", "cpstest.Test9m5_2e"),
       TestRun("testdata/set9Try/m5_3", "cpstest.Test9m5_3"),
-      TestRun("testdata/set9Try/m6_000", "cpstest.Test9m6_000")
-
+      TestRun("testdata/set9Try/m6_000", "cpstest.Test9m6_000"),
+      TestRun("testdata/set9Try/m6_000e", "cpstest.Test9m6_000e")
     )
   }
 
