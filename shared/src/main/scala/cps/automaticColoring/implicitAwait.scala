347cab2daf6fe7578399bb2f2f9172f73e98494a
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2023-06-10 16:43:48 +0300

    match first context argument in automatic-tag conversion

--- shared/src/main/scala/cps/automaticColoring/implicitAwait.scala
+++ shared/src/main/scala/cps/automaticColoring/implicitAwait.scala
@@ -5,7 +5,7 @@ import cps.*
 /**
  * implicit conversion which inserted during autmatic coloring when async value used in sync context.
  **/
-transparent inline given conversion[F[_],T,G[_]](using CpsMonadConversion[F,G], CpsMonadMemoization[F], AutomaticColoringTag[F], CpsMonadContext[G]): Conversion[F[T],T] =
+transparent inline given conversion[F[_],T,G[_]](using CpsMonadContext[G], CpsMonadConversion[F,G], CpsMonadMemoization[F], AutomaticColoringTag[F]): Conversion[F[T],T] =
            x => await[F,T,G](x)
 
 
