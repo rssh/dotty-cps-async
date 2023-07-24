d28747496595b87492ce42cda453e2f6cc2b14c1
alnkravchenko <alenakravchenko1@gmail.com>
2023-07-24 13:58:31 +0300

    change test case 3 to check transformation of function as return value

--- compiler-plugin/src/test/scala/cc/Test12.scala
+++ compiler-plugin/src/test/scala/cc/Test12.scala
@@ -19,7 +19,7 @@ class Test12 {
   def testCompileAndRunM2(): Unit =
     DotcInvocations.checkRuns(selection = (".*".r))(
       TestRun("testdata/set12/m2", "cpstest.Test12m2", "prefixmyurltransformed\n"),
-      TestRun("testdata/set12/m3", "cpstest.Test12m3", "prefixmyurltransformed/item\n")
+      TestRun("testdata/set12/m3", "cpstest.Test12m3", "myurl/itemtransformed\n")
     )
 
 }
