6b822565d6a68d3af35d7e4999919d2d67eacf79
alnkravchenko <alenakravchenko1@gmail.com>
2023-08-27 18:40:20 +0300

    fix tests

--- compiler-plugin/src/test/scala/cc/Test12.scala
+++ compiler-plugin/src/test/scala/cc/Test12.scala
@@ -10,20 +10,16 @@ class Test12 {
   @Test
   def testCompileAndRunM1(): Unit =
     val dotcInvocations = new DotcInvocations()
-    try {
-      val (code, output) =
+    val (code, output)  =
         dotcInvocations.compileAndRunFilesInDir("testdata/set12/m1", "cpstest.Test12m1")
-      val reporter       = dotcInvocations.reporter
+    val reporter        = dotcInvocations.reporter
       println("summary: " + reporter.summary)
       println(s"output=${output}")
-      assert(false, "Expected exception was not thrown")
-    } catch {
-      case ex: CpsTransformException =>
+    assert(code == 1, "Expected one exception to be thrown")
         assert(
-          ex.getMessage == "Object has to be a high-order function",
+      output == "Object has to be a high-order function",
           "There should be a CpsTransformException about high-order check"
         )
-    }
 
   @Test
   def testCompileAndRunM2(): Unit =
@@ -34,19 +30,15 @@ class Test12 {
   @Test
   def testCompileAndRunM3(): Unit =
     val dotcInvocations = new DotcInvocations()
-    try {
-      val (code, output) =
+    val (code, output)  =
         dotcInvocations.compileAndRunFilesInDir("testdata/set12/m3", "cpstest.Test12m3")
-      val reporter       = dotcInvocations.reporter
+    val reporter        = dotcInvocations.reporter
       println("summary: " + reporter.summary)
       println(s"output=${output}")
-      assert(false, "Expected exception was not thrown")
-    } catch {
-      case ex: CpsTransformException =>
+    assert(code == 1, "Expected one exception to be thrown")
         assert(
-          ex.getMessage == "Unsupported type of function. The return type must not be a function",
+      output == "Unsupported type of function. The return type must not be a function",
           "There should be a CpsTransformException about unsupported type"
         )
-    }
 
 }
