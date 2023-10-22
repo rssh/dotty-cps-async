4348305b140b46919c5a4430b193fdc57aef0e65
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2023-10-22 19:15:30 +0300

    added test for correct stack-trace from the hof argument

--- jvm/src/test/scala/cpstest/stacktraceReporting/TestBasicStacktraceReporting.scala
+++ jvm/src/test/scala/cpstest/stacktraceReporting/TestBasicStacktraceReporting.scala
@@ -16,7 +16,6 @@ class TestBasicStacktraceReporting {
       throw new RuntimeException("test")
       x+y
     }
-    val endPos = SourcePos.current
 
     val r = c.run()
     assert(r.isFailure)
@@ -25,8 +24,28 @@ class TestBasicStacktraceReporting {
         //ex.printStackTrace()
         //println(s"startPos = ${startPos}")
         assert(ex.getStackTrace().exists(_.getLineNumber() == startPos.line + 4))
+      case  Success(e) =>
+        assert(false)
   }
 
+  @Test
+  def testStackTraceFromHOFunArg() = {
+    val startPos = SourcePos.current
+    val c = async[ComputationBound] {
+      val x = List(1,2,3).map{ x =>
+         if (x%2) == 1 then throw new RuntimeException("test") else x + await(T1.cbi(1))
+      }
+      x
+    }
+    val r = c.run()
+    r match
+      case Failure(ex) =>
+        //ex.printStackTrace()
+        //println(s"startPos = ${startPos}")
+        assert(ex.getStackTrace().exists(_.getLineNumber() == startPos.line + 3))
+      case Success(e) =>
+        assert(false)
 
+  }
 
 }
