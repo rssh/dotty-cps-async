fb293844ef0753a477fc64e51627973fb859f938
Ivan Kyrylov <ivan.kyrylov@intellias.com>
2024-07-31 19:11:22 +0300

    Formatting

--- static-injection-examples/shared/src/main/scala/injection/examples/randomgen/plain/repository/Repository.scala
+++ static-injection-examples/shared/src/main/scala/injection/examples/randomgen/plain/repository/Repository.scala
@@ -2,6 +2,6 @@ package injection.examples.randomgen.plain.repository
 
 trait Repository[F[_], T] {
   def all: F[List[T]]
-  
+
   def count: F[Long]
 }
--- static-injection-examples/shared/src/main/scala/injection/examples/randomgen/simple/repository/Repository.scala
+++ static-injection-examples/shared/src/main/scala/injection/examples/randomgen/simple/repository/Repository.scala
@@ -2,6 +2,6 @@ package injection.examples.randomgen.simple.repository
 
 trait Repository[F[_], T] {
   def all: F[List[T]]
-  
+
   def count: F[Long]
 }
